59dae91fd797a8d09cc1573c00dcbd8ac2b204bd
Mark Wubben <mark@novemberborn.net>
2020-07-05 18:21:01 +0200

    fixup! Experimentally implement shared workers

--- test/shared-workers/supports-esm-workers/test.js
+++ test/shared-workers/supports-esm-workers/test.js
@@ -1,6 +1,12 @@
 const test = require('@ava/test');
 const exec = require('../../helpers/exec');
 
-test('can load CJS workers with a default export', async t => {
-	await t.notThrowsAsync(exec.fixture());
+test('can load ESM workers from file URL', async t => {
+	await t.notThrowsAsync(exec.fixture('file-url.js'));
 });
+
+if (process.platform !== 'win32') {
+	test('can load ESM workers from absolute posix path', async t => {
+		await t.notThrowsAsync(exec.fixture('posix-path.js'));
+	});
+}
